843173418a09dfac4a2d77ed2fe2376ac2070050
TotoAskeo <nullus.aspectus@gmail.com>
2017-08-18 09:55:16 +0300

    Updated main class

---
+++ src/CWDemo.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class CWDemo {
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				Window w = new Window();
+				w.setVisible(true);
+			}
+		});
+	}
+}
